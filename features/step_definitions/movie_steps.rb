70b1ef9968724290380c5a30b45967b21f9edaed
Kyran Lange <kyran.lange@gmail.com>
2012-03-16 08:31:47 -0700

    Added step to check if one string occurs before another string

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -12,9 +12,8 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
+  regexp = Regexp.new ".*#{e1}.*#{e2}"
+  page.body.should =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
