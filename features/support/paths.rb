702c89b2329cf7c09c2778bc1a0e75eee8363bcb
Kyran Lange <kyran.lange@gmail.com>
2012-03-20 03:37:51 -0700

    Finished scenario 1 of hw4 - Adding director field

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,12 @@ module NavigationHelpers
       '/'
     when /^the RottenPotatoes home page$/
       '/movies'
+    when /^the edit page for "(.*)"$/
+      m = Movie.find_by_title($1)
+      edit_movie_path(m)
+    when /^the details page for "(.*)"$/
+      m = Movie.find_by_title($1)
+      movie_path(m)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
