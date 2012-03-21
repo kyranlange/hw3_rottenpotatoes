0b1390567afab23890ce546b4b14dd1890521226
Kyran Lange <kyran.lange@gmail.com>
2012-03-21 05:38:20 -0700

    Implemented method on Model to find similar movies. Implemented sad path in controller

---
+++ spec/models/movies_spec.rb
@@ -0,0 +1,10 @@
+
+describe Movie do
+  describe 'find movies with same director' do
+    it 'should call database to find movies' do
+      Movie.should_receive(:find_by_director)
+      m = Movie.new(:title => "Movie1", :id => 1, :director => "bob")
+      m.find_similar
+    end
+  end
+end
