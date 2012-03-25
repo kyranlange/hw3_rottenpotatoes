39e6105d5e71012112cfc9fafc8cfe8a1fba4c2b
Kyran Lange <kyran.lange@gmail.com>
2012-03-25 00:43:10 -0700

    Fixed up rspec test

--- spec/models/movies_spec.rb
+++ spec/models/movies_spec.rb
@@ -2,7 +2,7 @@
 describe Movie do
   describe 'find movies with same director' do
     it 'should call database to find movies' do
-      Movie.should_receive(:find_by_director)
+      Movie.should_receive(:find_all_by_director)
       m = Movie.new(:title => "Movie1", :id => 1, :director => "bob")
       m.find_similar
     end
